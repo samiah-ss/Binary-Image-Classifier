497f8a8a4f58ed5f744ec75d62b2c0b754bbfdb5
Samiah <samsid813@gmail.com>
2026-08-06 21:46:47 -0700

    Update Site Description

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -99,6 +99,6 @@ if uploaded_file is not None:
 
 st.write("Made by Samiah Siddiqua")
 st.write(
-    "_The core model was presented alongside Jihyun Jeong, Polly Pao, and Jahnavi Gupta "
+    "_The core model was presented by Samiah Siddiqua alongside Jihyun Jeong, Polly Pao, and Jahnavi Gupta "
     "for Cyber-AI Camp's 2026 Cyber-AI Hackathon, where it won 3rd place._"
 )
